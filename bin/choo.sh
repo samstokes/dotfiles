34e34d9e0d93dd9637af05aa9a572d5e0f7e2b34
Sam Stokes <sam@mo.jo>
2009-01-05 17:25:17 +0000

    Refactor choo.sh

--- bin/choo.sh
+++ bin/choo.sh
@@ -2,16 +2,29 @@
 
 # Set up a Rails development environment, with a tabbed terminal and a gvim.
 
+GNOME_TERMINAL_PROFILE=Default
+
 error() { echo "$@" >&2; exit 1; }
 
-dirname=${1:-.}     # default to current dir if not specified
-test -d "$dirname" || error "$dirname is not a directory"
-proj=$(basename $(cd "$dirname" && pwd))
+parse_args() {
+  dirname=${1:-.}     # default to current dir if not specified
+  test -d "$dirname" || error "$dirname is not a directory"
+  proj=$(basename $(cd "$dirname" && pwd))
+}
 
-gnome_terminal_opts="--tab-with-profile=Default --working-directory=$dirname"
+launch_tabbed_terminal() {
+  local opts="--tab-with-profile=$GNOME_TERMINAL_PROFILE --working-directory=$dirname"
 
-/usr/bin/gnome-terminal \
-    $gnome_terminal_opts \
-    $gnome_terminal_opts -t "$proj - specs" -e /usr/bin/autotest \
+  /usr/bin/gnome-terminal \
+      $opts \
+      $opts -t "$proj - specs" -e /usr/bin/autotest \
 
-{ cd "$dirname" && gvim . ; }
+}
+
+launch_gvim_browser() {
+  { cd "$dirname" && gvim . ; }
+}
+
+parse_args "$@"
+launch_tabbed_terminal
+launch_gvim_browser
