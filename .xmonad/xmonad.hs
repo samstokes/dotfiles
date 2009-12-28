3749896045ccae82d53600a266f7f2d923f0c64a
Sam Stokes <me@samstokes.co.uk>
2009-12-28 19:52:33 +0000

    xmonad play nice with gnome-do (assuming Do is bound to a different key than normal)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,12 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.ManageHook
 
 main = xmonad $ gnomeConfig
     { modMask = mod4Mask
+    , manageHook = manageHook gnomeConfig <+> composeAll myManageHook
     }
+
+myManageHook :: [ManageHook]
+myManageHook =
+    [ resource =? "Do"   --> doIgnore ]
