68fcf2bbe8fe29082d57837f554f3a55363d298f
Sam Stokes <me@samstokes.co.uk>
2009-12-28 22:27:31 +0000

    Rearrange, and add keybindings for Gnome Do and Terminal

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,30 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.ManageHook
+import XMonad.Util.EZConfig
 
 myModMask = mod4Mask
 
-main = xmonad $ gnomeConfig
-    { modMask = myModMask
-    , manageHook = manageHook gnomeConfig <+> composeAll myManageHook
-    }
+myKeys :: [(String, X ())]
+myKeys =
+    [ ("M-S-p", spawn "gnome-do")
+    , ("M-t"  , spawn myTerminal)
+    ]
+
+myStartupHook :: X ()
+myStartupHook = checkKeymap myConfig myKeys
 
 myManageHook :: [ManageHook]
 myManageHook =
     [ resource =? "Do"   --> doIgnore ]
+
+myConfig = gnomeConfig
+    { modMask = myModMask
+    , startupHook = startupHook gnomeConfig >> myStartupHook
+    , manageHook = manageHook gnomeConfig <+> composeAll myManageHook
+    }
+    `additionalKeysP` myKeys
+
+myTerminal = terminal myConfig
+
+main = xmonad myConfig
