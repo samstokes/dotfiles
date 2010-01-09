574fe93f3b60e76c03b2c3673c6a88b499bce19b
Sam Stokes <me@samstokes.co.uk>
2010-01-09 05:32:54 +0000

    xmonad - use vim folding to make config more navigable

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,3 +1,5 @@
+-- vim:foldmethod=marker:foldcolumn=4
+-- === Imports === {{{1
 import Data.Monoid
 import XMonad
 import XMonad.Actions.Plane
@@ -13,19 +15,27 @@ import XMonad.Layout.ShowWName
 import XMonad.ManageHook
 import XMonad.Util.EZConfig
 
+
+-- === Keyboard and mouse === {{{1
+
 myModMask = mod4Mask
+
+----- Keyboard bindings ----- {{{2
+-- These are EZConfig emacs-style bindings
+-- (see http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Util-EZConfig.html)
+-- The startupHook (below) checks for syntax errors and duplicate bindings.
 
 myKeys :: [(String, X ())]
 myKeys =
-    -- launchers
+    ----- launchers ----- {{{3
     [ ("M-r",     gnomeRun)
     , ("M-S-r",   spawn "gnome-do")
 
-    -- tools and apps
+    ----- tools and apps ----- {{{3
     , ("M-p",     spawn "x nice top")
     , ("M-g"    , spawn "x ssh -t jabberwock.vm.bytemark.co.uk bin/passgrep")
 
-    -- workspace navigation
+    ----- workspace navigation ----- {{{3
     , ("C-M-h",   myMove ToLeft)
     , ("C-M-j",   myMove ToDown)
     , ("C-M-k",   myMove ToUp)
@@ -35,31 +45,36 @@ myKeys =
     , ("S-C-M-k", myShift ToUp)
     , ("S-C-M-l", myShift ToRight)
 
-    -- window management commands
+    ----- window management commands ----- {{{3
     , ("M-z",     withFocused (\win -> sendMessage (MinimizeWin win)))
     , ("S-M-z",   sendMessage RestoreNextMinimizedWin)
     , ("M1-<F4>", kill)
-
-      -- skip boring windows when changing focus
+      ----- skip boring windows when changing focus ----- {{{4
     , ("M-j",     focusDown)
     , ("M-k",     focusUp)
     , ("M-m",     focusMaster)
 
-    -- session management
+    ----- session management ----- {{{3
     , ("M-<F4>",  spawn "gnome-session-save --shutdown-dialog")
     ]
+    ----- helper functions ----- {{{3
     where
       myMove = planeMove GConf Finite
       myShift = planeShift GConf Finite
 
+----- Mouse bindings ----- {{{2
+
 myMouseBindings :: [((ButtonMask, Button), Window -> X ())]
 myMouseBindings =
     [
-      -- Super-scroll to transparentise window
+      ----- Super-scroll to transparentise window ----- {{{3
       -- Maybe useful if I ever set up conky?
       {-((myModMask, button4), fadeIn)-}
     {-, ((myModMask, button5), (flip setOpacity) 0.1)-}
     ]
+
+
+-- === Layout modifiers === {{{1
 
 myLayoutModifiers =
     boringAuto
@@ -68,29 +83,52 @@ myLayoutModifiers =
   . layoutHintsWithPlacement (0.5, 0.5)
   . noBorders
 
+
+-- === Workspace setup === {{{1
+
 myWorkspaces = ["mail", "read", "code"] ++ map show [4..9]
+
+
+-- === Event hook === {{{1
 
 myEventHook :: Event -> X All
 myEventHook = restoreMinimizedEventHook
 
+
+-- === Startup hook === {{{1
+
 myStartupHook :: X ()
 myStartupHook = checkKeymap myConfig myKeys
+
+
+-- === Manage hook === {{{1
 
 myManageHook :: [ManageHook]
 myManageHook = hookAll `concatMap` [ (doFloat,  floatables)
                                    , (doIgnore, ignorables)
                                    ]
   where
+    ----- windows to ignore completely (don't manage) {{{2
     ignorables = [ resource =? "Do"
                  ]
+    ----- windows to float {{{2
     floatables = [ className =? "Gcalctool"
                  ]
+    ----- helper functions ----- {{{2
     hookAll (hook, queries) = [query --> hook | query <- queries]
+
+
+-- === Log hook === {{{1
 
 myLogHook :: X ()
 myLogHook = do
   updatePointer $ TowardsCentre 0.2 0.2
   fadeInactiveLogHook 0.8
+
+
+-- === Put it all together === {{{1
+
+----- XConfig ----- {{{2
 
 myConfig = gnomeConfig
     { modMask = myModMask
@@ -105,5 +143,7 @@ myConfig = gnomeConfig
     `additionalMouseBindings` myMouseBindings
 
 myTerminal = terminal myConfig
+
+----- main function ----- {{{2
 
 main = xmonad myConfig
