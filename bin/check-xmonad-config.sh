349b54550c62214e2283e8e47699da40593b72ff
Sam Stokes <me@samstokes.co.uk>
2010-12-23 01:01:00 -0800

    check-xmonad-config.sh - fix script now xmonad.hs loads other modules

--- bin/check-xmonad-config.sh
+++ bin/check-xmonad-config.sh
@@ -1,2 +1,2 @@
 XMONAD_CONF_DIR="$HOME"/.xmonad
-echo ":l $XMONAD_CONF_DIR/xmonad.hs" | ghci -i$XMONAD_CONF_DIR/lib | grep 'Ok, modules loaded: Main'
+echo ":l $XMONAD_CONF_DIR/xmonad.hs" | ghci -i$XMONAD_CONF_DIR/lib | grep 'Ok, modules loaded: .*Main'
