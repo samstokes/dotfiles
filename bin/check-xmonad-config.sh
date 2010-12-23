527a5ea7e1f8e567c49c3726b3eb05bbcd06725d
Sam Stokes <me@samstokes.co.uk>
2010-12-23 01:02:19 -0800

    check-xmonad-config.sh - make sure script uses same GHC as xmonad

--- bin/check-xmonad-config.sh
+++ bin/check-xmonad-config.sh
@@ -1,2 +1,3 @@
 XMONAD_CONF_DIR="$HOME"/.xmonad
-echo ":l $XMONAD_CONF_DIR/xmonad.hs" | ghci -i$XMONAD_CONF_DIR/lib | grep 'Ok, modules loaded: .*Main'
+GHCI=/usr/bin/ghci         # same as xmonad's own recompile will use
+echo ":l $XMONAD_CONF_DIR/xmonad.hs" | "$GHCI" -i$XMONAD_CONF_DIR/lib | grep 'Ok, modules loaded: .*Main'
