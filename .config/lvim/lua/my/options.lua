f45bd9777902abc7350db91461015ce3935bdbed
Sam Stokes <me@samstokes.co.uk>
2022-10-01 22:05:05 -0700

    lvim - improve some editor options

--- .config/lvim/lua/my/options.lua
+++ .config/lvim/lua/my/options.lua
@@ -1,3 +1,7 @@
 local o = vim.o
 
 o.wildmode = 'longest,list'
+
+-- override lunarvim settings
+o.wrap = true
+o.whichwrap = 'b,s' -- Don't want Left/Right wrapping to next line. Restore vim default.
