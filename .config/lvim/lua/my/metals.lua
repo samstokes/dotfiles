890e9723304f3e59c2bd5901d75122684f45fac8
Sam Stokes <sstokes@launchdarkly.com>
2022-10-05 13:57:24 -0700

    lvim - mappings for metals tree view

--- .config/lvim/lua/my/metals.lua
+++ .config/lvim/lua/my/metals.lua
@@ -11,10 +11,16 @@ vim.api.nvim_create_autocmd("FileType", {
     local metals_config = metals.bare_config()
     metals_config.on_attach = function(client, bufnr)
       require("lvim.lsp").common_on_attach(client, bufnr)
+      local wk = require("which-key")
+
       -- TODO maybe bind this to metals.run_scalafix
-      -- local wk = require("which-key")
       -- wk.register({ ["go"] = { "<cmd>lua require('metals').organize_imports()<CR>", "Organize Imports" } },
       --   { mode = "n", buffer = bufnr })
+
+      wk.register({
+        ["<Leader>lv"] = { function() require('metals.tvp').reveal_in_tree() end, "Reveal in Tree" },
+        ["<Leader>lt"] = { function() require('metals.tvp').toggle_tree_view() end, "Toggle Tree View" },
+      })
 
       -- TODO maybe centralize this?
       require("metals").setup_dap()
