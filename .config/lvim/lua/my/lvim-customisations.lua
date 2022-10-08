2d4af28501c963caa24a871b2d55d185caec6efe
Sam Stokes <me@samstokes.co.uk>
2022-10-08 15:25:40 -0700

    lvim - message when cd'ing to project root

--- .config/lvim/lua/my/lvim-customisations.lua
+++ .config/lvim/lua/my/lvim-customisations.lua
@@ -3,4 +3,5 @@
 vim.o.wrap = true
 vim.o.whichwrap = 'b,s' -- Don't want Left/Right wrapping to next line. Restore vim default.
 
+lvim.builtin.project.silent_chdir = false
 lvim.builtin.terminal.insert_mappings = false -- don't clobber <C-t> in insert mode
