82ad288632770f2ec20ce5fd151c49f3ea0ed904
Sam Stokes <me@samstokes.co.uk>
2022-10-10 22:14:38 -0700

    lvim - disable autopairs, I tried but I can't

--- .config/lvim/lua/my/lvim-customisations.lua
+++ .config/lvim/lua/my/lvim-customisations.lua
@@ -3,6 +3,7 @@
 vim.o.wrap = true
 vim.o.whichwrap = 'b,s' -- Don't want Left/Right wrapping to next line. Restore vim default.
 
+lvim.builtin.autopairs.active = false
 lvim.builtin.project.silent_chdir = false
 lvim.builtin.terminal.insert_mappings = false -- don't clobber <C-t> in insert mode
 
