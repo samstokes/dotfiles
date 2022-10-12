55897d320eff299d8ec5b01ec469913470677b88
Sam Stokes <me@samstokes.co.uk>
2022-10-11 22:00:26 -0700

    lvim - add keymaps file, bind Alt-hjkl for window switching

--- .config/lvim/lua/my/keymaps.lua
+++ .config/lvim/lua/my/keymaps.lua
@@ -1,1 +1,12 @@
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', {})
+
+local normal_mappings = {
+  ["<A-h>"] = "<C-w>h",
+  ["<A-j>"] = "<C-w>j",
+  ["<A-k>"] = "<C-w>k",
+  ["<A-l>"] = "<C-w>l",
+}
+
+for lhs, rhs in pairs(normal_mappings) do
+  vim.keymap.set('n', lhs, rhs, {})
+end
