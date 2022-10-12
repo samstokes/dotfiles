55ae56e0c2666264c06fc91cc639f98818115bce
Sam Stokes <me@samstokes.co.uk>
2022-10-11 21:59:17 -0700

    lvim - remove weird <C-s> mapping

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -26,7 +26,6 @@ lvim.colorscheme = "gruvbox"
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
