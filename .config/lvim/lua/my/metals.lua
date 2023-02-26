26c76c39c23180ed581942fe19c94db4c38ebaac
Sam Stokes <sstokes@launchdarkly.com>
2023-02-25 16:35:30 -0800

    lvim - add more mappings for scala-metals

--- .config/lvim/lua/my/metals.lua
+++ .config/lvim/lua/my/metals.lua
@@ -13,16 +13,17 @@ vim.api.nvim_create_autocmd("FileType", {
     local metals_config = metals.bare_config()
     metals_config.on_attach = function(client, bufnr)
       require("lvim.lsp").common_on_attach(client, bufnr)
+
       local wk = require("which-key")
-
-      -- TODO maybe bind this to metals.run_scalafix
-      -- wk.register({ ["go"] = { "<cmd>lua require('metals').organize_imports()<CR>", "Organize Imports" } },
-      --   { mode = "n", buffer = bufnr })
-
       wk.register({
-        ["<Leader>lv"] = { function() require('metals.tvp').reveal_in_tree() end, "Reveal in Tree" },
-        ["<Leader>lt"] = { function() require('metals.tvp').toggle_tree_view() end, "Toggle Tree View" },
-      })
+        m = {
+          name = "+Metals",
+          f = { function() require('metals').run_scalafix() end, "Organize Imports" },
+          v = { function() require('metals.tvp').reveal_in_tree() end, "Reveal in Tree" },
+          t = { function() require('metals.tvp').toggle_tree_view() end, "Toggle Tree View" },
+          K = { function() require('metals').hover_worksheet() end, "Show full result in worksheet" },
+        },
+      }, lvim.builtin.which_key.opts)
 
       -- TODO maybe centralize this?
       require("metals").setup_dap()
