9d372b9aec264a099fa02f7e31b21dbdb71360a5
Sam Stokes <me@samstokes.co.uk>
2023-12-20 16:04:20 -0800

    lvim - move leap config into keymaps

--- .config/lvim/lua/my/plugins.lua
+++ .config/lvim/lua/my/plugins.lua
@@ -4,29 +4,6 @@ lvim.plugins = {
   -- vimwiki and vim-notes are added in vimwiki.lua
   { "chaoren/vim-wordmotion" },
   { "tpope/vim-unimpaired" },
+  { "ggandor/leap.nvim" },
   { "kiyoon/telescope-insert-path.nvim" },
-  {
-    "ggandor/leap.nvim",
-    config = function()
-      require('leap')
-
-      -- based on set_default_keymaps in leap:lua/leap/user.lua
-      -- but omitting gs (cross-window search) to avoid conflict with LSP bindings
-      local keymaps = {
-        { "n", "s", "<Plug>(leap-forward)" },
-        { "n", "S", "<Plug>(leap-backward)" },
-        { "x", "s", "<Plug>(leap-forward)" },
-        { "x", "S", "<Plug>(leap-backward)" }, { "o", "z", "<Plug>(leap-forward)" },
-        { "o", "Z", "<Plug>(leap-backward)" },
-        { "o", "x", "<Plug>(leap-forward-x)" },
-        { "o", "X", "<Plug>(leap-backward-x)" },
-      }
-      for _, keymap in ipairs(keymaps) do
-        local mode = keymap[1]
-        local lhs = keymap[2]
-        local rhs = keymap[3]
-        vim.keymap.set(mode, lhs, rhs, { silent = true })
-      end
-    end,
-  },
 }
