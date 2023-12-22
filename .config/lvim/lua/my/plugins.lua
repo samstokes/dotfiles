ebf39dcabcf9bd593ccd6e8cd3c3c99851f4d3cf
Sam Stokes <me@samstokes.co.uk>
2023-12-22 15:24:14 -0800

    lvim - even more reformatting

--- .config/lvim/lua/my/plugins.lua
+++ .config/lvim/lua/my/plugins.lua
@@ -1,17 +1,17 @@
 lvim.plugins = {
-  { "lifepillar/vim-solarized8" },
-  { "tpope/vim-surround" },
-  -- vimwiki and vim-notes are added in vimwiki.lua
-  { "chaoren/vim-wordmotion" },
-  { "tpope/vim-unimpaired" },
-  { "ggandor/leap.nvim" },
-  { "ggandor/leap-ast.nvim" },
-  { "kiyoon/telescope-insert-path.nvim" },
-  {
-      "ruifm/gitlinker.nvim",
-      requires = "nvim-lua/plenary.nvim",
-      config = function()
-        require 'gitlinker'.setup()
-      end,
-  },
+    { "lifepillar/vim-solarized8" },
+    { "tpope/vim-surround" },
+    -- vimwiki and vim-notes are added in vimwiki.lua
+    { "chaoren/vim-wordmotion" },
+    { "tpope/vim-unimpaired" },
+    { "ggandor/leap.nvim" },
+    { "ggandor/leap-ast.nvim" },
+    { "kiyoon/telescope-insert-path.nvim" },
+    {
+        "ruifm/gitlinker.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        config = function()
+          require 'gitlinker'.setup()
+        end,
+    },
 }
