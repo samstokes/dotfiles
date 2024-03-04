ce08adb456ec6202f3663513acb6ad44c388c950
Sam Stokes <me@samstokes.co.uk>
2024-03-03 16:59:44 -0800

    lvim - reformat stuff

--- .config/lvim/lua/my/keymaps.lua
+++ .config/lvim/lua/my/keymaps.lua
@@ -22,13 +22,13 @@ if success then
   -- but omitting gs (cross-window search) to avoid conflict with LSP bindings
   -- and replacing visual and operator-pending s/S with z/Z to avoid conflict with vim-surround
   local leap_keymaps = {
-    {{"n"}, "s", "<Plug>(leap-forward-to)", "Leap forward to"},
-    {{"n"}, "S", "<Plug>(leap-backward-to)", "Leap backward to"},
-    {{"x", "o"}, "z", "<Plug>(leap-forward-to)", "Leap forward to"},
-    {{"x", "o"}, "Z", "<Plug>(leap-backward-to)", "Leap backward to"},
-    {{"x", "o"}, "x", "<Plug>(leap-forward-till)", "Leap forward till"},
-    {{"x", "o"}, "X", "<Plug>(leap-backward-till)", "Leap backward till"},
-    {{"n", "x", "o"}, "\\", function() require'leap-ast'.leap() end, "Leap AST"},
+    { { "n" }, "s", "<Plug>(leap-forward-to)", "Leap forward to" },
+    { { "n" }, "S", "<Plug>(leap-backward-to)", "Leap backward to" },
+    { { "x", "o" }, "z", "<Plug>(leap-forward-to)", "Leap forward to" },
+    { { "x", "o" }, "Z", "<Plug>(leap-backward-to)", "Leap backward to" },
+    { { "x", "o" }, "x", "<Plug>(leap-forward-till)", "Leap forward till" },
+    { { "x", "o" }, "X", "<Plug>(leap-backward-till)", "Leap backward till" },
+    { { "n", "x", "o" }, "\\", function() require 'leap-ast'.leap() end, "Leap AST" },
   }
 
   for _, keymap in ipairs(leap_keymaps) do
@@ -37,7 +37,7 @@ if success then
     local rhs = keymap[3]
     local desc = keymap[4]
     for _, mode in ipairs(modes) do
-      vim.keymap.set(mode, lhs, rhs, {silent = true, desc = desc})
+      vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
     end
   end
 end
--- .config/lvim/lua/my/plugins.lua
+++ .config/lvim/lua/my/plugins.lua
@@ -1,24 +1,24 @@
 lvim.plugins = {
-    { "lifepillar/vim-solarized8" },
-    { "tpope/vim-surround" },
-    -- vimwiki and vim-notes are added in vimwiki.lua
-    { "chaoren/vim-wordmotion" },
-    { "tpope/vim-unimpaired" },
-    { "ggandor/leap.nvim" },
-    { "ggandor/leap-ast.nvim" },
-    { "kiyoon/telescope-insert-path.nvim" },
-    {
-        "ruifm/gitlinker.nvim",
-        dependencies = "nvim-lua/plenary.nvim",
-        config = function()
-          require 'gitlinker'.setup()
-        end,
-    },
-    {
-      "samstokes/telescope-todo",
-      dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-      config = function()
-        require 'telescope-todo'.setup()
-      end,
-    },
+  { "lifepillar/vim-solarized8" },
+  { "tpope/vim-surround" },
+  -- vimwiki and vim-notes are added in vimwiki.lua
+  { "chaoren/vim-wordmotion" },
+  { "tpope/vim-unimpaired" },
+  { "ggandor/leap.nvim" },
+  { "ggandor/leap-ast.nvim" },
+  { "kiyoon/telescope-insert-path.nvim" },
+  {
+    "ruifm/gitlinker.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require 'gitlinker'.setup()
+    end,
+  },
+  {
+    "samstokes/telescope-todo",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      require 'telescope-todo'.setup()
+    end,
+  },
 }
