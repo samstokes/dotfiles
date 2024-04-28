787e64829837c131e7d2ebc4f01a2e1277107eaa
Sam Stokes <me@samstokes.co.uk>
2024-04-27 17:28:34 -0700

    nvim - add Rust LSP

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -10,6 +10,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
+        "rust_analyzer",
         -- add more arguments for adding more language servers
       })
     end,
