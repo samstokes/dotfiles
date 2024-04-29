7ed1b7dd746d736bbb58a0f90cfb7702ba1f335b
Sam Stokes <me@samstokes.co.uk>
2024-04-28 18:30:33 -0700

    nvim - add leap and leap-ast

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -1,85 +1,31 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- You can also add or configure plugins by creating files in this `plugins/` folder
--- Here are some examples:
-
 ---@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
-
-  "andweeb/presence.nvim",
   {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
-
-  -- == Examples of Overriding Plugins ==
-
-  -- customize alpha options
-  {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      -- customize the dashboard header
-      opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
+    "ggandor/leap.nvim",
+    config = function()
+      -- based on set_default_keymaps in leap:lua/leap/user.lua
+      -- but omitting gs (cross-window search) to avoid conflict with LSP bindings
+      -- and replacing visual and operator-pending s/S with z/Z to avoid conflict with vim-surround
+      local leap_keymaps = {
+        { { "n" }, "s", "<Plug>(leap-forward-to)", "Leap forward to" },
+        { { "n" }, "S", "<Plug>(leap-backward-to)", "Leap backward to" },
+        { { "x", "o" }, "z", "<Plug>(leap-forward-to)", "Leap forward to" },
+        { { "x", "o" }, "Z", "<Plug>(leap-backward-to)", "Leap backward to" },
+        { { "x", "o" }, "x", "<Plug>(leap-forward-till)", "Leap forward till" },
+        { { "x", "o" }, "X", "<Plug>(leap-backward-till)", "Leap backward till" },
+        { { "n", "x", "o" }, "\\", function() require("leap-ast").leap() end, "Leap AST" },
       }
-      return opts
+
+      for _, keymap in ipairs(leap_keymaps) do
+        local modes = keymap[1]
+        local lhs = keymap[2]
+        local rhs = keymap[3]
+        local desc = keymap[4]
+        for _, mode in ipairs(modes) do
+          vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
+        end
+      end
     end,
   },
-
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
-
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
-
-  {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
-    end,
-  },
+  { "ggandor/leap-ast.nvim" },
 }
