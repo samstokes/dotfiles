6e7317fbc3c9544fcf11ef92946ec6faf23f997d
Sam Stokes <sstokes@launchdarkly.com>
2024-11-04 17:55:29 -0800

    clean up bin a bit

--- bin/borders.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -XPOST https://192.168.1.1:8080/cgi-bin/login.cgi -df_user=bordersguest -df_pass=bordersguest -dsubmit=
--- bin/rails_find_unspecced.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-for i in `cd app; find * -type f`; do test -f spec/${i%.rb}_spec.rb || echo $i; done
