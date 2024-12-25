f3ff1f36517b7ccde94d0ac74913ff9f6fde2727
Sam Stokes <sstokes@launchdarkly.com>
2024-12-24 17:00:34 -0800

    lvim - remove A-hjkl for window nav since it won't work on Mac

--- .config/lvim/lua/my/keymaps.lua
+++ .config/lvim/lua/my/keymaps.lua
@@ -1,15 +1,5 @@
+vim.g.maplocalleader = ';'
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', {})
-
-local normal_mappings = {
-  ["<A-h>"] = "<C-w>h",
-  ["<A-j>"] = "<C-w>j",
-  ["<A-k>"] = "<C-w>k",
-  ["<A-l>"] = "<C-w>l",
-}
-
-for lhs, rhs in pairs(normal_mappings) do
-  vim.keymap.set('n', lhs, rhs, {})
-end
 
 lvim.builtin.which_key.mappings["t"] = {
   "<cmd>Telescope todo<cr>",
