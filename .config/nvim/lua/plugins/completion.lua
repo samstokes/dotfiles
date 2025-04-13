c17f541a90516806af78f3159c59c65d8ae006e3
Sam Stokes <me@samstokes.co.uk>
2025-04-12 22:54:48 -0700

    nvim - tweak Blink completion to unbreak Enter key

---
+++ .config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        list = {
+          selection = {
+            -- unbreak Enter key
+            preselect = false,
+          },
+        },
+      },
+    },
+  },
+}
