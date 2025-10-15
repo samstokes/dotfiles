5f1146d03209240ccc986890f4b5754ccb22c065
Sam Stokes <me@samstokes.co.uk>
2025-10-14 17:27:22 -0700

    nvim - add Markdown preview command

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- for Markdown, <Leader>rp to do :w !pandoc | browser-preview
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    local map = LazyVim.safe_keymap_set
+    map("n", "<Leader>rp", "<Cmd>w !pandoc | browser-preview<CR>", { desc = "Pandoc to Browser Preview" })
+  end,
+})
