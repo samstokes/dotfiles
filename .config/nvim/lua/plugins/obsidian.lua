7b0f570cbf0334f49390fd7711e53c6a3b01adde
Sam Stokes <sam@eventualcomputing.com>
2025-11-12 13:01:35 -0800

    nvim - add obsidian.nvim

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,31 @@
+local vault = "~/Notes"
+local vault_expanded = vim.fn.expand(vault)
+
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  -- only load obsidian.nvim for markdown files in the vault:
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    -- refer to `:h file-pattern` for more examples
+    "BufReadPre "
+      .. vault_expanded
+      .. "/*.md",
+    "BufNewFile " .. vault_expanded .. "/*.md",
+  },
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    workspaces = {
+      {
+        name = "work",
+        path = vault,
+      },
+    },
+
+    daily_notes = {
+      folder = "diary",
+    },
+  },
+}
